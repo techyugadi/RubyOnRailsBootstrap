1a8418655f9c9eb212c3715ad19507e65bd5acf3
TechYugadi <techyugadi@gmail.com>
2020-02-06 18:59:33 +0530

    corrrected scriptdir and yarn install

--- centos/setup.sh
+++ centos/setup.sh
@@ -12,7 +12,7 @@ RAILS_VER=6.0.1
 MYSQL_VER=80
 APPNAME=myapp
 
-scriptdir=`pwd`
+scriptdir=`dirname "$(readlink -f "$0")"`
 
 sudo yum install -y curl
 
@@ -69,6 +69,7 @@ cd $APPNAME
 appdir=`pwd`
 rake db:create
 
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 sudo yum install -y yarn
 rails webpacker:install
 # Start the app
